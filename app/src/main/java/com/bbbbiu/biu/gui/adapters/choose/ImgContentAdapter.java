0c45c4ca52d9c7edf8aad22b2d76791973a48cda
YieldNull <yieldnull@gmail.com>
2016-04-18 14:29:24 +0800

    fix img ui bug

--- app/src/main/java/com/bbbbiu/biu/gui/adapters/choose/ImgContentAdapter.java
+++ app/src/main/java/com/bbbbiu/biu/gui/adapters/choose/ImgContentAdapter.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 import com.bbbbiu.biu.R;
@@ -18,7 +19,6 @@ import com.squareup.picasso.Picasso;
 import com.yqritc.recyclerviewflexibledivider.HorizontalDividerItemDecoration;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -49,11 +49,12 @@ public class ImgContentAdapter extends ContentBaseAdapter implements HorizontalD
         this.context = context;
         mPicasso = Picasso.with(context);
 
-
+        // 计算图片宽度，高度
         float screenWidth = SizeUtil.getScreenWidth(context);
-        float dpMargin = SizeUtil.convertDpToPixel(4);
+        float dpMargin = SizeUtil.convertDpToPixel(2);
         mImgWidth = (int) (screenWidth - dpMargin * 2) / 3;
 
+        // tint place holder
         Drawable drawable = context.getResources().getDrawable(R.drawable.img_placeholder);
         mPlaceholder = DrawableCompat.wrap(drawable);
         DrawableCompat.setTint(mPlaceholder, context.getResources().getColor(R.color.img_placeholder));
@@ -159,9 +160,12 @@ public class ImgContentAdapter extends ContentBaseAdapter implements HorizontalD
                     .tag(PICASSO_TAG)
                     .resize(mImgWidth, mImgWidth)
                     .placeholder(mPlaceholder)
-//                    .onlyScaleDown()
+                    .onlyScaleDown()
                     .centerCrop()
                     .into(holder.imageView);
+
+            // 设置图片大小，不然高度会出现问题，会多出来一点点
+            holder.imageView.setLayoutParams(new FrameLayout.LayoutParams(mImgWidth, mImgWidth));
 
             holder.itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -176,7 +180,7 @@ public class ImgContentAdapter extends ContentBaseAdapter implements HorizontalD
             } else if (getChosenCount() == 0) {
                 holder.chosenImage.setImageDrawable(null);
             } else {
-                holder.chosenImage.setImageDrawable(context.getResources().getDrawable(R.drawable.ic_chossing_circle));
+                holder.chosenImage.setImageDrawable(context.getResources().getDrawable(R.drawable.ic_choosing_circle));
             }
 
         } else {
