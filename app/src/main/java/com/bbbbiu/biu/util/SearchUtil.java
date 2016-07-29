05e662136df4ddf580b33c8bee0b9beddd79fbfa
YieldNull <yieldnull@gmail.com>
2016-07-30 00:21:51 +0800

    query file using extension instead of MIME

--- app/src/main/java/com/bbbbiu/biu/util/SearchUtil.java
+++ app/src/main/java/com/bbbbiu/biu/util/SearchUtil.java
@@ -7,23 +7,20 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.media.MediaMetadataRetriever;
-import android.mtp.MtpConstants;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.util.Log;
-import android.webkit.MimeTypeMap;
 
 import com.bbbbiu.biu.db.search.ApkItem;
 import com.bbbbiu.biu.db.search.FileItem;
-import com.bbbbiu.biu.db.search.ModelItem;
 import com.bbbbiu.biu.db.search.MediaItem;
+import com.bbbbiu.biu.db.search.ModelItem;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 
@@ -342,35 +339,27 @@ public class SearchUtil {
     /**
      * 从MediaStore读取相应后缀名的文件路径,剔除不存在的文件
      *
-     * @param context   context
-     * @param extension 后缀名列表
+     * @param context    context
+     * @param extensions 后缀名列表
      * @return 文件列表
      */
-    private static Set<String> scanFileWithExtension(Context context, List<String> extension) {
+    private static Set<String> scanFileWithExtension(Context context, List<String> extensions) {
         Set<String> resultSet = new HashSet<>();
 
-        Set<String> mimeList = new HashSet<>();
-
-        for (String ext : extension) {
-            String mime = MediaFile.getMimeTypeForFile("." + ext);
-            if (mime != null) {
-                mimeList.add(mime);
-            }
+        List<String> args = new ArrayList<>();
+        for (String ext : extensions) {
+            args.add("%." + ext);
         }
 
-        if (mimeList.size() == 0) {
+        if (args.size() == 0) {
             return resultSet;
         }
 
+
         StringBuilder selectionBuilder = new StringBuilder();
-        int count = 0;
-        for (String ignored : mimeList) {
-            if (count != 0) {
-                selectionBuilder.append(" OR " + MediaStore.Files.FileColumns.MIME_TYPE + "=?");
-            } else {
-                selectionBuilder.append(MediaStore.Files.FileColumns.MIME_TYPE + "=?");
-            }
-            count++;
+        selectionBuilder.append(MediaStore.Files.FileColumns.DATA + " LIKE ?");
+        for (int i = 1; i < args.size(); i++) {
+            selectionBuilder.append(" OR " + MediaStore.Files.FileColumns.DATA + " LIKE ?");
         }
 
         ContentResolver contentResolver = context.getContentResolver();
@@ -380,9 +369,10 @@ public class SearchUtil {
                 uri,
                 new String[]{MediaStore.Files.FileColumns.DATA},
                 selectionBuilder.toString(),
-                mimeList.toArray(new String[mimeList.size()]),
+                args.toArray(new String[args.size()]),
                 null
         );
+
 
         if (cursor == null) {
             return resultSet;
@@ -475,198 +465,4 @@ public class SearchUtil {
         return min + ":" + sec.trim().substring(0, 2);
     }
 
-    /**
-     * 用{@link MimeTypeMap }计算得到的“PPT”的MIME TYPE与 MediaStore查到的不同。故直接用其源码算了
-     * <p/>
-     * Android MediaScanner Helper Class.
-     * <p/>
-     * Part of the source of <a href="https://android.googlesource.com/platform/frameworks/base/+/cd92588/media/java/android/media/MediaFile.java">android.media.MediaFile</a>
-     * <p/>
-     */
-    public static class MediaFile {
-        // Audio file types
-        public static final int FILE_TYPE_MP3 = 1;
-        public static final int FILE_TYPE_M4A = 2;
-        public static final int FILE_TYPE_WAV = 3;
-        public static final int FILE_TYPE_AMR = 4;
-        public static final int FILE_TYPE_AWB = 5;
-        public static final int FILE_TYPE_WMA = 6;
-        public static final int FILE_TYPE_OGG = 7;
-        public static final int FILE_TYPE_AAC = 8;
-        public static final int FILE_TYPE_MKA = 9;
-        public static final int FILE_TYPE_FLAC = 10;
-        private static final int FIRST_AUDIO_FILE_TYPE = FILE_TYPE_MP3;
-        private static final int LAST_AUDIO_FILE_TYPE = FILE_TYPE_FLAC;
-        // MIDI file types
-        public static final int FILE_TYPE_MID = 11;
-        public static final int FILE_TYPE_SMF = 12;
-        public static final int FILE_TYPE_IMY = 13;
-        private static final int FIRST_MIDI_FILE_TYPE = FILE_TYPE_MID;
-        private static final int LAST_MIDI_FILE_TYPE = FILE_TYPE_IMY;
-
-        // Video file types
-        public static final int FILE_TYPE_MP4 = 21;
-        public static final int FILE_TYPE_M4V = 22;
-        public static final int FILE_TYPE_3GPP = 23;
-        public static final int FILE_TYPE_3GPP2 = 24;
-        public static final int FILE_TYPE_WMV = 25;
-        public static final int FILE_TYPE_ASF = 26;
-        public static final int FILE_TYPE_MKV = 27;
-        public static final int FILE_TYPE_MP2TS = 28;
-        public static final int FILE_TYPE_AVI = 29;
-        public static final int FILE_TYPE_WEBM = 30;
-        private static final int FIRST_VIDEO_FILE_TYPE = FILE_TYPE_MP4;
-        private static final int LAST_VIDEO_FILE_TYPE = FILE_TYPE_WEBM;
-
-        // More video file types
-        public static final int FILE_TYPE_MP2PS = 200;
-        private static final int FIRST_VIDEO_FILE_TYPE2 = FILE_TYPE_MP2PS;
-        private static final int LAST_VIDEO_FILE_TYPE2 = FILE_TYPE_MP2PS;
-        // Image file types
-        public static final int FILE_TYPE_JPEG = 31;
-        public static final int FILE_TYPE_GIF = 32;
-        public static final int FILE_TYPE_PNG = 33;
-        public static final int FILE_TYPE_BMP = 34;
-        public static final int FILE_TYPE_WBMP = 35;
-        public static final int FILE_TYPE_WEBP = 36;
-        private static final int FIRST_IMAGE_FILE_TYPE = FILE_TYPE_JPEG;
-        private static final int LAST_IMAGE_FILE_TYPE = FILE_TYPE_WEBP;
-
-        // Playlist file types
-        public static final int FILE_TYPE_M3U = 41;
-        public static final int FILE_TYPE_PLS = 42;
-        public static final int FILE_TYPE_WPL = 43;
-        public static final int FILE_TYPE_HTTPLIVE = 44;
-        private static final int FIRST_PLAYLIST_FILE_TYPE = FILE_TYPE_M3U;
-        private static final int LAST_PLAYLIST_FILE_TYPE = FILE_TYPE_HTTPLIVE;
-        // Drm file types
-        public static final int FILE_TYPE_FL = 51;
-        private static final int FIRST_DRM_FILE_TYPE = FILE_TYPE_FL;
-        private static final int LAST_DRM_FILE_TYPE = FILE_TYPE_FL;
-        // Other popular file types
-        public static final int FILE_TYPE_TEXT = 100;
-        public static final int FILE_TYPE_HTML = 101;
-        public static final int FILE_TYPE_PDF = 102;
-        public static final int FILE_TYPE_XML = 103;
-        public static final int FILE_TYPE_MS_WORD = 104;
-        public static final int FILE_TYPE_MS_EXCEL = 105;
-        public static final int FILE_TYPE_MS_POWERPOINT = 106;
-        public static final int FILE_TYPE_ZIP = 107;
-
-        public static class MediaFileType {
-            public final int fileType;
-            public final String mimeType;
-
-            MediaFileType(int fileType, String mimeType) {
-                this.fileType = fileType;
-                this.mimeType = mimeType;
-            }
-        }
-
-        private static final HashMap<String, MediaFileType> sFileTypeMap
-                = new HashMap<String, MediaFileType>();
-        private static final HashMap<String, Integer> sMimeTypeMap
-                = new HashMap<String, Integer>();
-        // maps file extension to MTP format code
-        private static final HashMap<String, Integer> sFileTypeToFormatMap
-                = new HashMap<String, Integer>();
-        // maps mime type to MTP format code
-        private static final HashMap<String, Integer> sMimeTypeToFormatMap
-                = new HashMap<String, Integer>();
-        // maps MTP format code to mime type
-        private static final HashMap<Integer, String> sFormatToMimeTypeMap
-                = new HashMap<Integer, String>();
-
-        static void addFileType(String extension, int fileType, String mimeType) {
-            sFileTypeMap.put(extension, new MediaFileType(fileType, mimeType));
-            sMimeTypeMap.put(mimeType, Integer.valueOf(fileType));
-        }
-
-        static void addFileType(String extension, int fileType, String mimeType, int mtpFormatCode) {
-            addFileType(extension, fileType, mimeType);
-            sFileTypeToFormatMap.put(extension, Integer.valueOf(mtpFormatCode));
-            sMimeTypeToFormatMap.put(mimeType, Integer.valueOf(mtpFormatCode));
-            sFormatToMimeTypeMap.put(mtpFormatCode, mimeType);
-        }
-
-        static {
-            addFileType("MP3", FILE_TYPE_MP3, "audio/mpeg", MtpConstants.FORMAT_MP3);
-            addFileType("MPGA", FILE_TYPE_MP3, "audio/mpeg", MtpConstants.FORMAT_MP3);
-            addFileType("M4A", FILE_TYPE_M4A, "audio/mp4", MtpConstants.FORMAT_MPEG);
-            addFileType("WAV", FILE_TYPE_WAV, "audio/x-wav", MtpConstants.FORMAT_WAV);
-            addFileType("AMR", FILE_TYPE_AMR, "audio/amr");
-            addFileType("AWB", FILE_TYPE_AWB, "audio/amr-wb");
-
-            addFileType("OGG", FILE_TYPE_OGG, "audio/ogg", MtpConstants.FORMAT_OGG);
-            addFileType("OGG", FILE_TYPE_OGG, "application/ogg", MtpConstants.FORMAT_OGG);
-            addFileType("OGA", FILE_TYPE_OGG, "application/ogg", MtpConstants.FORMAT_OGG);
-            addFileType("AAC", FILE_TYPE_AAC, "audio/aac", MtpConstants.FORMAT_AAC);
-            addFileType("AAC", FILE_TYPE_AAC, "audio/aac-adts", MtpConstants.FORMAT_AAC);
-            addFileType("MKA", FILE_TYPE_MKA, "audio/x-matroska");
-
-            addFileType("MID", FILE_TYPE_MID, "audio/midi");
-            addFileType("MIDI", FILE_TYPE_MID, "audio/midi");
-            addFileType("XMF", FILE_TYPE_MID, "audio/midi");
-            addFileType("RTTTL", FILE_TYPE_MID, "audio/midi");
-            addFileType("SMF", FILE_TYPE_SMF, "audio/sp-midi");
-            addFileType("IMY", FILE_TYPE_IMY, "audio/imelody");
-            addFileType("RTX", FILE_TYPE_MID, "audio/midi");
-            addFileType("OTA", FILE_TYPE_MID, "audio/midi");
-            addFileType("MXMF", FILE_TYPE_MID, "audio/midi");
-
-            addFileType("MPEG", FILE_TYPE_MP4, "video/mpeg", MtpConstants.FORMAT_MPEG);
-            addFileType("MPG", FILE_TYPE_MP4, "video/mpeg", MtpConstants.FORMAT_MPEG);
-            addFileType("MP4", FILE_TYPE_MP4, "video/mp4", MtpConstants.FORMAT_MPEG);
-            addFileType("M4V", FILE_TYPE_M4V, "video/mp4", MtpConstants.FORMAT_MPEG);
-            addFileType("3GP", FILE_TYPE_3GPP, "video/3gpp", MtpConstants.FORMAT_3GP_CONTAINER);
-            addFileType("3GPP", FILE_TYPE_3GPP, "video/3gpp", MtpConstants.FORMAT_3GP_CONTAINER);
-            addFileType("3G2", FILE_TYPE_3GPP2, "video/3gpp2", MtpConstants.FORMAT_3GP_CONTAINER);
-            addFileType("3GPP2", FILE_TYPE_3GPP2, "video/3gpp2", MtpConstants.FORMAT_3GP_CONTAINER);
-            addFileType("MKV", FILE_TYPE_MKV, "video/x-matroska");
-            addFileType("WEBM", FILE_TYPE_WEBM, "video/webm");
-            addFileType("TS", FILE_TYPE_MP2TS, "video/mp2ts");
-            addFileType("AVI", FILE_TYPE_AVI, "video/avi");
-
-            addFileType("JPG", FILE_TYPE_JPEG, "image/jpeg", MtpConstants.FORMAT_EXIF_JPEG);
-            addFileType("JPEG", FILE_TYPE_JPEG, "image/jpeg", MtpConstants.FORMAT_EXIF_JPEG);
-            addFileType("GIF", FILE_TYPE_GIF, "image/gif", MtpConstants.FORMAT_GIF);
-            addFileType("PNG", FILE_TYPE_PNG, "image/png", MtpConstants.FORMAT_PNG);
-            addFileType("BMP", FILE_TYPE_BMP, "image/x-ms-bmp", MtpConstants.FORMAT_BMP);
-            addFileType("WBMP", FILE_TYPE_WBMP, "image/vnd.wap.wbmp");
-            addFileType("WEBP", FILE_TYPE_WEBP, "image/webp");
-
-            addFileType("M3U", FILE_TYPE_M3U, "audio/x-mpegurl", MtpConstants.FORMAT_M3U_PLAYLIST);
-            addFileType("M3U", FILE_TYPE_M3U, "application/x-mpegurl", MtpConstants.FORMAT_M3U_PLAYLIST);
-            addFileType("PLS", FILE_TYPE_PLS, "audio/x-scpls", MtpConstants.FORMAT_PLS_PLAYLIST);
-            addFileType("WPL", FILE_TYPE_WPL, "application/vnd.ms-wpl", MtpConstants.FORMAT_WPL_PLAYLIST);
-            addFileType("M3U8", FILE_TYPE_HTTPLIVE, "application/vnd.apple.mpegurl");
-            addFileType("M3U8", FILE_TYPE_HTTPLIVE, "audio/mpegurl");
-            addFileType("M3U8", FILE_TYPE_HTTPLIVE, "audio/x-mpegurl");
-            addFileType("FL", FILE_TYPE_FL, "application/x-android-drm-fl");
-            addFileType("TXT", FILE_TYPE_TEXT, "text/plain", MtpConstants.FORMAT_TEXT);
-            addFileType("HTM", FILE_TYPE_HTML, "text/html", MtpConstants.FORMAT_HTML);
-            addFileType("HTML", FILE_TYPE_HTML, "text/html", MtpConstants.FORMAT_HTML);
-            addFileType("PDF", FILE_TYPE_PDF, "application/pdf");
-            addFileType("DOC", FILE_TYPE_MS_WORD, "application/msword", MtpConstants.FORMAT_MS_WORD_DOCUMENT);
-            addFileType("XLS", FILE_TYPE_MS_EXCEL, "application/vnd.ms-excel", MtpConstants.FORMAT_MS_EXCEL_SPREADSHEET);
-            addFileType("PPT", FILE_TYPE_MS_POWERPOINT, "application/mspowerpoint", MtpConstants.FORMAT_MS_POWERPOINT_PRESENTATION);
-            addFileType("FLAC", FILE_TYPE_FLAC, "audio/flac", MtpConstants.FORMAT_FLAC);
-            addFileType("ZIP", FILE_TYPE_ZIP, "application/zip");
-            addFileType("MPG", FILE_TYPE_MP2PS, "video/mp2p");
-            addFileType("MPEG", FILE_TYPE_MP2PS, "video/mp2p");
-        }
-
-        private static MediaFileType getFileType(String path) {
-            int lastDot = path.lastIndexOf('.');
-            if (lastDot < 0)
-                return null;
-            return sFileTypeMap.get(path.substring(lastDot + 1).toUpperCase(Locale.ROOT));
-        }
-
-        public static String getMimeTypeForFile(String path) {
-            MediaFileType mediaFileType = getFileType(path);
-            return (mediaFileType == null ? null : mediaFileType.mimeType);
-        }
-
-    }
 }
